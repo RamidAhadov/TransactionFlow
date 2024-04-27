2e9019641ddb2b28e81431347a0ef5fa559bf8e6
RamidAhadov <ramidehedov02@gmail.com>
2024-04-27 21:27:52 +0400

    Lock mechanism added

--- TransactionFlow.BillingSystem/Services/Concrete/SessionService.cs
+++ TransactionFlow.BillingSystem/Services/Concrete/SessionService.cs
@@ -5,14 +5,30 @@ namespace TransactionFlow.BillingSystem.Services.Concrete;
 public class SessionService:ISessionService
 {
     private static readonly Dictionary<string, object?> cache = new();
+    private Dictionary<string, object> locks = new();
     
     public void Set(string key, object? value)
     {
-        cache.TryAdd(key, value ?? new object());
+        lock (GetLockObject(key))
+        {
+            cache.TryAdd(key, value ?? new object());
+        }
     }
 
     public object? Get(string key)
     {
         return cache.GetValueOrDefault(key);
+    }
+    
+    private object GetLockObject(string key)
+    {
+        lock (locks)
+        {
+            if (!locks.ContainsKey(key))
+            {
+                locks[key] = new object();
+            }
+            return locks[key];
+        }
     }
 }
