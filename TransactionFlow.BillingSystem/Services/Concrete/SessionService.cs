6378e0a9e7216e3e761cd1a292a15fa82e6b093f
RamidAhadov <ramidehedov02@gmail.com>
2024-05-01 06:39:28 +0400

    Session service switched from local store to db store

--- TransactionFlow.BillingSystem/Services/Concrete/SessionService.cs
+++ TransactionFlow.BillingSystem/Services/Concrete/SessionService.cs
@@ -1,23 +1,48 @@
+using AutoMapper;
+using TransactionFlow.BillingSystem.Models.Dtos;
 using TransactionFlow.BillingSystem.Services.Abstraction;
+using TransactionFlow.Business.Abstraction;
 
 namespace TransactionFlow.BillingSystem.Services.Concrete;
 
 public class SessionService:ISessionService
 {
-    private static readonly Dictionary<string, object?> cache = new();
+    private IMemoryManager _memoryManager;
+    private IMapper _mapper;
     private Dictionary<string, object> locks = new();
-    
+
+    public SessionService(IMemoryManager memoryManager, IMapper mapper)
+    {
+        _memoryManager = memoryManager;
+        _mapper = mapper;
+    }
+
     public void Set(string key, object? value)
     {
         lock (GetLockObject(key))
         {
-            cache.TryAdd(key, value ?? new object());
+            _ = _memoryManager.SetKey(key);
         }
     }
 
-    public object? Get(string key)
+    public string? Get(string key)
     {
-        return cache.GetValueOrDefault(key);
+        var keyResult = _memoryManager.GetValueByKey(key);
+        if (keyResult.IsSuccess)
+        {
+            var idempotencyKey = _mapper.Map<IdempotencyKeyDto>(keyResult.Value);
+            if (idempotencyKey != null)
+            {
+                if (idempotencyKey.Value == null)
+                {
+                    return string.Empty;
+                }
+                
+                return idempotencyKey.Value;
+            }
+        }
+
+        return null;
     }
     
     private object GetLockObject(string key)
