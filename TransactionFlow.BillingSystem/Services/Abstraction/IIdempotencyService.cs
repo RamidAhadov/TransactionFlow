bc3f99e58bdf01356d294eb3e904c12a1124545d
RamidAhadov <ramidehedov02@gmail.com>
2024-05-07 03:02:32 +0400

    Set method with new key parameters

--- TransactionFlow.BillingSystem/Services/Abstraction/IIdempotencyService.cs
+++ TransactionFlow.BillingSystem/Services/Abstraction/IIdempotencyService.cs
@@ -1,7 +1,11 @@
+using System.Net;
+
 namespace TransactionFlow.BillingSystem.Services.Abstraction;
 
 public interface IIdempotencyService
 {
-    void Set(string key, object? value = default);
+    void Set(HttpRequest request, HttpStatusCode responseCode, object requestBody, string? responseBody = default);
     string? Get(string key);
+    string GenerateKey();
+    string GenerateKey(params string[] parameters);
 }
--- TransactionFlow.BillingSystem/Services/Concrete/IdempotencyService.cs
+++ TransactionFlow.BillingSystem/Services/Concrete/IdempotencyService.cs
@@ -1,8 +1,12 @@
 using System.Collections.Concurrent;
+using System.Net;
+using System.Text;
 using AutoMapper;
+using NuGet.Protocol;
 using TransactionFlow.BillingSystem.Models.Dtos;
 using TransactionFlow.BillingSystem.Services.Abstraction;
 using TransactionFlow.Business.Abstraction;
+using TransactionFlow.Business.Models;
 
 namespace TransactionFlow.BillingSystem.Services.Concrete;
 
@@ -18,16 +22,39 @@ public class IdempotencyService:IIdempotencyService
         _mapper = mapper;
     }
 
-    public void Set(string key, object? value)
+    public void Set(HttpRequest request, HttpStatusCode responseCode, object requestBody,
+        string? responseBody = default)
     {
+        var key = request.Headers["Idempotency-key"].ToString();
+        
+        if (!request.Body.CanSeek)
+        {
+            request.EnableBuffering();
+        }
+        
+        var idempotencyKey = new IdempotencyKeyDto
+        {
+            Key = key,
+            RequestMethod = request.Method,
+            RequestPath = request.Path,
+            RequestParameters = requestBody.ToJson(),
+            ResponseCode = (int)responseCode,
+            ResponseBody = responseBody
+        };
+        
         lock (GetLockObject(key))
         {
-            _ = _memoryManager.SetKey(key);
+            _ = _memoryManager.SetKey(_mapper.Map<IdempotencyKeyModel>(idempotencyKey));
         }
     }
 
     public string? Get(string key)
     {
+        if (string.IsNullOrEmpty(key))
+        {
+            throw new ArgumentNullException();
+        }
+        
         lock (GetLockObject(key))
         {
             var keyResult = _memoryManager.GetValueByKey(key);
@@ -36,19 +63,28 @@ public class IdempotencyService:IIdempotencyService
                 var idempotencyKey = _mapper.Map<IdempotencyKeyDto>(keyResult.Value);
                 if (idempotencyKey != null)
                 {
-                    if (idempotencyKey.Value == null)
+                    if (idempotencyKey.ResponseBody == null)
                     {
                         return string.Empty;
                     }
                 
-                    return idempotencyKey.Value;
+                    return idempotencyKey.ResponseBody;
                 }
             }
 
             return null;
         }
     }
-    
+    public string GenerateKey()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GenerateKey(params string[] parameters)
+    {
+        throw new NotImplementedException();
+    }
+
     private object GetLockObject(string key)
     {
         return _locks.GetOrAdd(key, _ => new object());
