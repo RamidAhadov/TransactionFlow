544c99750be2d16f40747eef210577df936ba025
ramidahadov <ramidehedov02@gmail.com>
2024-05-22 08:41:39 +0400

    Unused log classes deleted

--- TransactionFlow.Core/CrossCuttingConcerns/Logging/LogDetail.cs
+++
@@ -1,8 +0,0 @@
-namespace TransactionFlow.Core.CrossCuttingConcerns.Logging;
-
-public class LogDetail
-{
-    public string FullName { get; set; }
-    public string MethodName { get; set; }
-    public List<LogParameter> Parameters { get; set; }
-}
